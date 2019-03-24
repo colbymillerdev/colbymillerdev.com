ff5875acdfd266d2f77f28a05394034354e076a4
Colby Miller <colbymillerdev@gmail.com>
2019-03-23 22:26:45 -0400

    Updated main picture on site.

--- src/components/ProfilePic.js
+++ src/components/ProfilePic.js
@@ -4,13 +4,14 @@ import styled from 'styled-components';
 import ProfessionalPic from '../../assets/ProfessionalPic.jpg';
 
 const CircleImage = styled.div`
-    width: 330px;
-    height: 330px;
+    width: 300px;
+    height: 300px;
     border-radius: 50%;
     -webkit-border-radius: 50%;
     -moz-border-radius: 50%;
     background: url("${ProfessionalPic}") no-repeat;
-    background-size: 330px 330px;
+    background-size: cover;
+    background-position: 50% 30%;
     margin-right: auto;
     margin-left: auto;
 `;
